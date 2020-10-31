348b0a60940a2327e8053c88b3d7ae1dc5a0bdf4
KC Kai <kirstenckai@gmail.com>
2020-10-31 13:43:49 -0700

    Fix bug in input field

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const form = document.getElementById('form')
 const validateEmail = () => {
   form.addEventListener('submit', (e) => {
   e.preventDefault()
-  if (email === '' || email === null) {
+  if ($(email).val() === '' || $(email).val() === null) {
     $('.error').show().delay(4000).fadeOut();
+    $(email).addClass('error-outline');
   } else {
     $('.error').hide();
+    $(email).removeClass('error-outline');
   }
 })
 }
